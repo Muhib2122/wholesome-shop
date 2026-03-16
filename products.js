f8dd03d9dde87ef717d4c19307f596edd6e26616
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-16 15:01:42 +0600

    Update products.js

---
+++ products.js
@@ -0,0 +1,13 @@
+const products = [
+
+{ name:"Rice", price:120 },
+
+{ name:"Milk", price:80 },
+
+{ name:"Lipstick", price:500 },
+
+{ name:"Shari", price:2500 },
+
+{ name:"Shirt", price:1200 }
+
+]
