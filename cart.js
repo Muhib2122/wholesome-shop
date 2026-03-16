3ff52b6439aa64b02adabda510ed416674748049
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-16 21:38:29 +0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -2,21 +2,18 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function addToCart(name, price){
 
-    cart.push({name, price});
+cart.push({name, price});
 
-    // save cart in browser
-    localStorage.setItem("cart", JSON.stringify(cart));
+localStorage.setItem("cart", JSON.stringify(cart));
 
-    // update cart count
-    document.getElementById("cart-count").innerText = cart.length;
+document.getElementById("cart-count").innerText = cart.length;
 
-    alert(name + " added to cart");
+alert(name + " added to cart");
 
 }
 
 function goCheckout(){
 
-    // go to checkout page
-    window.location.href = "checkout.html";
+window.location.href = "cart.html";
 
 }
