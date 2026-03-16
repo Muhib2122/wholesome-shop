5da1305837f7eaf2ab69ea5b737b553f535cc8ad
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-16 19:12:18 +0600

    Update shop.js

--- shop.js
+++ shop.js
@@ -7,7 +7,7 @@ let div = document.createElement("div");
 div.className = "product";
 
 div.innerHTML = `
-<img src="${product.img}" class="product-img">
+<img src="${product.img}" style="width:150px;height:150px;object-fit:cover">
 
 <h3>${product.name}</h3>
 
