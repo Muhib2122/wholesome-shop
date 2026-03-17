939f9139254016ac3d3c2fb86859fb5ccaca2e76
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-17 13:11:44 +0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -6,14 +6,46 @@ cart.push({name, price});
 
 localStorage.setItem("cart", JSON.stringify(cart));
 
-document.getElementById("cart-count").innerText = cart.length;
-
-alert(name + " added to cart");
+updateCart();
 
 }
 
-function goCheckout(){
+function updateCart(){
 
-window.location.href = "cart.html";
+document.getElementById("cart-count").innerText = cart.length;
+
+let cartItems = document.getElementById("cartItems");
+
+cartItems.innerHTML = "";
+
+let total = 0;
+
+cart.forEach(item => {
+
+let div = document.createElement("div");
+
+div.innerText = item.name + " - " + item.price;
+
+cartItems.appendChild(div);
+
+total += item.price;
+
+});
+
+document.getElementById("total").innerText = "Total: " + total + " BDT";
+
+}
+
+function openCart(){
+
+document.getElementById("cartPanel").classList.add("open");
+
+updateCart();
+
+}
+
+function closeCart(){
+
+document.getElementById("cartPanel").classList.remove("open");
 
 }
