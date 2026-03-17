0c3e28277df122b2c0c105c462e3de152b66d662
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-17 14:54:22 +0600

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,14 +1,76 @@
+// LOAD PRODUCTS
+const productContainer = document.getElementById("products");
+
+// FUNCTION: SHOW PRODUCTS
+function renderProducts(data){
+
+productContainer.innerHTML = "";
+
+data.forEach(product => {
+
+let div = document.createElement("div");
+
+div.className = "product";
+
+div.innerHTML = `
+
+<img src="${product.img}" 
+class="product-img"
+onerror="this.src='https://via.placeholder.com/150'">
+
+<h3>${product.name}</h3>
+
+<p>${product.price} BDT</p>
+
+<button onclick="addToCart('${product.name}',${product.price})">
+Add to Cart
+</button>
+
+`;
+
+productContainer.appendChild(div);
+
+});
+
+}
+
+// FIRST LOAD
+renderProducts(products);
+
+
+// 🔥 CATEGORY FILTER
 function filterCategory(cat){
 
-let filtered = products.filter(p => cat==="all" || p.category===cat);
+if(cat === "all"){
+renderProducts(products);
+return;
+}
+
+let filtered = products.filter(p => p.category === cat);
 
 renderProducts(filtered);
 
 }
 
+
+// 🔥 SEARCH
+function searchProduct(){
+
+let input = document.getElementById("search").value.toLowerCase();
+
+let filtered = products.filter(p =>
+p.name.toLowerCase().includes(input)
+);
+
+renderProducts(filtered);
+
+}
+
+
+// 🔥 SORT BY PRICE
 function sortPrice(){
 
-let sorted = [...products].sort((a,b)=>a.price-b.price);
+let sorted = [...products].sort((a,b)=>a.price - b.price);
 
 renderProducts(sorted);
 
