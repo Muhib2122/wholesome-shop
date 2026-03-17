ae8a341e4bada0bef5c294e5cbc020a77634f2f5
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-17 17:01:03 +0600

    Update products.js

--- products.js
+++ products.js
@@ -1,54 +1,48 @@
-const defaultProducts = [
+const products = [
 
 {
+id:1,
 name:"Rice",
 price:120,
 img:"rice.jpg",
-category:"groceries"
+category:"groceries",
+rating:4
 },
 
 {
+id:2,
 name:"Milk",
 price:80,
 img:"milk.jpg",
-category:"groceries"
+category:"groceries",
+rating:4
 },
 
 {
+id:3,
 name:"Lipstick",
 price:500,
 img:"lipstick.jpg",
-category:"makeup"
+category:"makeup",
+rating:5
 },
 
 {
+id:4,
 name:"Shari",
 price:2500,
 img:"shari.jpg",
-category:"clothing"
+category:"clothing",
+rating:5
 },
 
 {
+id:5,
 name:"Shirt",
 price:1200,
 img:"shirt.jpg",
-category:"clothing"
+category:"clothing",
+rating:4
 }
 
 ];
-
-// ADMIN PRODUCTS
-let stored = JSON.parse(localStorage.getItem("products")) || [];
-
-// MERGE
-const products = [...defaultProducts];
-
-stored.forEach(newP => {
-
-let exists = products.find(p => p.name === newP.name);
-
-if(!exists){
-products.push(newP);
-}
-
-});
