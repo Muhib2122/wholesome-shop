95a673f2780845bcb0935dbf70409e876bc488e6
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-17 17:40:01 +0600

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,7 @@
-const products = [
+const products = JSON.parse(localStorage.getItem("products")) || [
 
 {
+id:1,
 name:"Rice",
 price:120,
 img:"rice.jpg",
@@ -8,6 +9,7 @@ category:"groceries"
 },
 
 {
+id:2,
 name:"Milk",
 price:80,
 img:"milk.jpg",
@@ -15,6 +17,7 @@ category:"groceries"
 },
 
 {
+id:3,
 name:"Lipstick",
 price:500,
 img:"lipstick.jpg",
@@ -22,6 +25,7 @@ category:"makeup"
 },
 
 {
+id:4,
 name:"Shari",
 price:2500,
 img:"shari.jpg",
@@ -29,89 +33,16 @@ category:"clothing"
 },
 
 {
+id:5,
 name:"Shirt",
 price:1200,
 img:"shirt.jpg",
 category:"clothing"
-},
-
-// 🔥 NEW PRODUCTS (তুমি যেগুলো add করছো)
-
-{
-name:"clip",
-price:35,
-img:"https://i.imgur.com/clip.jpg",
-category:"makeup"
-},
-
-{
-name:"hair band",
-price:150,
-img:"https://i.imgur.com/hairband.jpg",
-category:"makeup"
-},
-
-{
-name:"car toy",
-price:160,
-img:"https://i.imgur.com/cartoy.jpg",
-category:"toys"
-},
-
-{
-name:"dog toy",
-price:1800,
-img:"https://i.imgur.com/dogtoy.jpg",
-category:"toys"
-},
-
-{
-name:"cool water pot",
-price:250,
-img:"https://i.imgur.com/pot.jpg",
-category:"home"
-},
-
-{
-name:"ceramic bowl",
-price:450,
-img:"https://i.imgur.com/bowl.jpg",
-category:"home"
-},
-
-{
-name:"bowl",
-price:350,
-img:"https://i.imgur.com/bowl2.jpg",
-category:"home"
-},
-
-{
-name:"3 piece dress",
-price:1750,
-img:"https://i.imgur.com/dress.jpg",
-category:"clothing"
-},
-
-{
-name:"premium dress",
-price:1550,
-img:"https://i.imgur.com/dress2.jpg",
-category:"clothing"
-},
-
-{
-name:"makeup box",
-price:1050,
-img:"https://i.imgur.com/makeupbox.jpg",
-category:"makeup"
-},
-
-{
-name:"cosmetic organizer",
-price:2250,
-img:"https://i.imgur.com/organizer.jpg",
-category:"makeup"
 }
 
 ];
+
+// 🔥 save initial if empty
+if(!localStorage.getItem("products")){
+localStorage.setItem("products", JSON.stringify(products));
+}
