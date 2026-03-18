d624986f9bfeab4c4b44ec50355ce9e5afe71438
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-18 17:39:11 +0600

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,10 +1,21 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const container = document.getElementById("products");
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "wholesome-shop-f4d0f.firebaseapp.com",
+  projectId: "wholesome-shop-f4d0f",
+  storageBucket: "wholesome-shop-f4d0f.appspot.com",
+  messagingSenderId: "301482399120",
+  appId: "1:301482399120:web:xxxx"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 async function loadProducts() {
   const querySnapshot = await getDocs(collection(db, "products"));
+  const container = document.getElementById("product-list");
 
   container.innerHTML = "";
 
@@ -12,13 +23,11 @@ async function loadProducts() {
     const p = doc.data();
 
     container.innerHTML += `
-      <div class="product">
-        <img src="${p.img}" class="product-img">
+      <div class="card">
+        <img src="${p.img}" width="100%">
         <h3>${p.name}</h3>
         <p>${p.price} BDT</p>
-        <button onclick="addToCart('${p.name}', ${p.price})">
-          Add to Cart
-        </button>
+        <button onclick="addToCart('${p.name}', ${p.price})">Add to Cart</button>
       </div>
     `;
   });
