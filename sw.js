b7859ff5c71bf1bc0f42ec21db58b385192c8a6e
muhib ibn shams <155169719+Muhib2122@users.noreply.github.com>
2026-03-20 02:48:32 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-self.addEventListener("install", e=>{
-console.log("SW Installed");
+self.addEventListener("install", e => {
+  self.skipWaiting(); // 🔥 important
 });
 
-self.addEventListener("fetch", e=>{});
+self.addEventListener("activate", e => {
+  console.log("SW Activated");
+});
+
+self.addEventListener("fetch", e => {
+  // no cache (safe)
+});
